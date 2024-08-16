0e6b9c20a0e858ad3acf11aaf9fbe4d5339b90b5
ingryd-victoria-cardoso <129208186+ingryd-victoria-cardoso@users.noreply.github.com>
2024-08-16 10:02:08 -0300

    s

--- script.js
+++ script.js
@@ -88,17 +88,28 @@ const perguntas = [
 let atual = 0;
 let perguntaAtual;
 
-function mostraAlternativas() {
-    for (const alternativa of perguntaAtual.alternativas) {
+function mostraPergunta(){
+   perguntaAtual = perguntas [atual];
+   caixaPerguntas.textContent =perguntaAtual.enunciado;
+   mostraAlternativas();
+}
+
+function mostraAlternativas(){
+  for(const alternativa of perguntaAtual.alternativas) {
       const botaoAlternativas = document.createElement("button");
       botaoAlternativas.textContent = alternativa.texto;
-      botaoAlternativas.addEventListener("click", function () {
-        atual++;
-        mostraPergunta();
-      });
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
       caixaAlternativas.appendChild(botaoAlternativas);
-    }
   }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal = afirmacoes;
+  atual++;
+  mostraPergunta();
+}
+mostraPergunta();
   
 
 
